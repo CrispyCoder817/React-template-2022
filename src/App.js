a184a1dff8275fee52fea5c785459f793e5d4bfb
Brandon G.M <brandongmerritt@gmail.com>
2022-10-15 18:38:25 -0500

    Copy and Pasta for best results

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import Version from "./Components/version";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          A quick clonable repo for your next React App. 
+          <br></br>
+          Please refer to the documentation below for offical documentation.
+          <br></br>
+          <Version/>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/version.js
@@ -0,0 +1,14 @@
+let VersionData = JSON.Stringify("react-dom");
+
+function Version(VersionData) {
+    return(
+        <div>
+            <p>
+                (this.VersionData);
+            </p>
+                
+        </div>
+    )
+}
+
+export default  Version();
